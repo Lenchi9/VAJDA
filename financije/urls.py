33fc738bdb278d3a53d2b99ce66a3650f5650d1a
Leon Vajda <leonvajda@Mac.fritz.box>
2026-02-21 05:58:57 +0100

    CRUD za sve i sve rad

--- financije/urls.py
+++ financije/urls.py
@@ -1,20 +1,28 @@
 from django.urls import path
 from .views import (
-    TransactionListView,
-    TransactionDetailView,
-    TransactionCreateView,
-    TransactionUpdateView,
-    TransactionDeleteView,
+    TransactionListView, TransactionDetailView,
+    TransactionCreateView, TransactionUpdateView, TransactionDeleteView,
+    AccountListView, AccountCreateView, AccountUpdateView, AccountDeleteView,
+    CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView,
     register,
 )
 
 urlpatterns = [
     path("", TransactionListView.as_view(), name="transaction_list"),
     path("transaction/<int:pk>/", TransactionDetailView.as_view(), name="transaction_detail"),
-
     path("transaction/new/", TransactionCreateView.as_view(), name="transaction_create"),
     path("transaction/<int:pk>/edit/", TransactionUpdateView.as_view(), name="transaction_update"),
     path("transaction/<int:pk>/delete/", TransactionDeleteView.as_view(), name="transaction_delete"),
+
+    path("accounts/", AccountListView.as_view(), name="account_list"),
+    path("accounts/new/", AccountCreateView.as_view(), name="account_create"),
+    path("accounts/<int:pk>/edit/", AccountUpdateView.as_view(), name="account_update"),
+    path("accounts/<int:pk>/delete/", AccountDeleteView.as_view(), name="account_delete"),
+
+    path("categories/", CategoryListView.as_view(), name="category_list"),
+    path("categories/new/", CategoryCreateView.as_view(), name="category_create"),
+    path("categories/<int:pk>/edit/", CategoryUpdateView.as_view(), name="category_update"),
+    path("categories/<int:pk>/delete/", CategoryDeleteView.as_view(), name="category_delete"),
 
     path("register/", register, name="register"),
 ]
--- financije/views.py
+++ financije/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from .models import Transaction
+from .models import Transaction, Account, Category
 
 
 def register(request):
@@ -38,7 +38,6 @@ class TransactionDetailView(LoginRequiredMixin, DetailView):
     template_name = "financije/transaction_detail.html"
 
     def get_queryset(self):
-        # spriječi da user vidi tuđu transakciju preko /transaction/<pk>/
         return Transaction.objects.filter(user=self.request.user)
 
 
@@ -47,6 +46,12 @@ class TransactionCreateView(LoginRequiredMixin, CreateView):
     template_name = "financije/transaction_form.html"
     fields = ["account", "category", "datum", "iznos", "opis"]
     success_url = reverse_lazy("transaction_list")
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields["account"].queryset = form.fields["account"].queryset.filter(user=self.request.user)
+        form.fields["category"].queryset = form.fields["category"].queryset.filter(user=self.request.user)
+        return form
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -61,6 +66,12 @@ class TransactionUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_queryset(self):
         return Transaction.objects.filter(user=self.request.user)
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields["account"].queryset = form.fields["account"].queryset.filter(user=self.request.user)
+        form.fields["category"].queryset = form.fields["category"].queryset.filter(user=self.request.user)
+        return form
 
 
 class TransactionDeleteView(LoginRequiredMixin, DeleteView):
@@ -70,3 +81,81 @@ class TransactionDeleteView(LoginRequiredMixin, DeleteView):
 
     def get_queryset(self):
         return Transaction.objects.filter(user=self.request.user)
+    
+
+class AccountListView(LoginRequiredMixin, ListView):
+    model = Account
+    template_name = "financije/account_list.html"
+    context_object_name = "accounts"
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user).order_by("naziv")
+
+
+class AccountCreateView(LoginRequiredMixin, CreateView):
+    model = Account
+    template_name = "financije/account_form.html"
+    fields = ["naziv", "tip", "pocetno_stanje"]
+    success_url = reverse_lazy("account_list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class AccountUpdateView(LoginRequiredMixin, UpdateView):
+    model = Account
+    template_name = "financije/account_form.html"
+    fields = ["naziv", "tip", "pocetno_stanje"]
+    success_url = reverse_lazy("account_list")
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user)
+
+
+class AccountDeleteView(LoginRequiredMixin, DeleteView):
+    model = Account
+    template_name = "financije/account_confirm_delete.html"
+    success_url = reverse_lazy("account_list")
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user)
+
+
+class CategoryListView(LoginRequiredMixin, ListView):
+    model = Category
+    template_name = "financije/category_list.html"
+    context_object_name = "categories"
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user).order_by("naziv")
+
+
+class CategoryCreateView(LoginRequiredMixin, CreateView):
+    model = Category
+    template_name = "financije/category_form.html"
+    fields = ["naziv", "tip"]
+    success_url = reverse_lazy("category_list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class CategoryUpdateView(LoginRequiredMixin, UpdateView):
+    model = Category
+    template_name = "financije/category_form.html"
+    fields = ["naziv", "tip"]
+    success_url = reverse_lazy("category_list")
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+
+class CategoryDeleteView(LoginRequiredMixin, DeleteView):
+    model = Category
+    template_name = "financije/category_confirm_delete.html"
+    success_url = reverse_lazy("category_list")
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
